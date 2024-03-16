f9d924f5d47b2b169866a56cacf3bbf2c0936591
Brix Suficiencia <brixsuficiencia@gmail.com>
2024-03-16 10:32:50 +0800

    dsadad

---
+++ pokedex/src/Pokedex.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Pokemon from './Pokemon';
+
+function Pokedex({ pokemonList }) {
+    const [language, setLanguage] = useState('english');
+
+    const handleLanguageChange = (selectedLanguage) => {
+        setLanguage(selectedLanguage);
+    };
+
+    return (
+        <div className="pokedex-container">
+            <div className="language-buttons">
+                <button class-name = "btnDesign" onClick={() => handleLanguageChange('english')}>English</button>
+                <button class-name = "btnDesign" onClick={() => handleLanguageChange('japanese')}>Japanese</button>
+                <button class-name = "btnDesign" onClick={() => handleLanguageChange('chinese')}>Chinese</button>
+                <button class-name = "btnDesign" onClick={() => handleLanguageChange('french')}>French</button>
+            </div>
+            <div className="pokemon-list">
+                {pokemonList.map((pokemon, index) => (
+                    <Pokemon
+                        key={index}
+                        id={pokemon.id}
+                        name={pokemon.name[language]}
+                        image={pokemon.image}
+                        types={pokemon.type}
+                        hp={pokemon.base.HP}
+                        attack={pokemon.base.Attack}
+                        defense={pokemon.base.Defense}
+                        spattack={pokemon.base["Sp. Attack"]}
+                        spdefense={pokemon.base["Sp. Defense"]}
+                        speed={pokemon.base.Speed}
+                        
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Pokedex;
---
+++ pokedex/src/Pokemon.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Pokemon({ id, name, image, types, hp, attack, defense, spattack, spdefense, speed}) {
+    return (
+        <div className="pokemon-card">
+            <div className="pokemon-image-container">
+                <img src={image} alt={name} className="pokemon-image" />
+            </div>
+            <div className="pokemon-info">
+                <p id="col1">[{id}] {name}</p>
+                <div className="types-container">
+                    {types && types.length > 0 && <p>{types.join('           ')}</p>}
+                </div>
+                <p>HP: {hp}</p>
+                <p>Attack: {attack}</p>
+                <p>Defense: {defense}</p>
+                <div className='row2'>
+                <p>Sp. Attack: {spattack}</p>
+                <p>Sp. Defense: {spdefense}</p>
+                <p>Speed: {speed}</p>
+
+                </div>
+            </div>
+        </div>
+    );
+}
